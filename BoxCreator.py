c284f835c808b2b72d79cc701adb058e236c847d
bradandrews510 <bradandrews510@gmail.com>
2018-09-07 18:11:39 -0500

    Added cool features like bomb placement

--- BoxCreator.py
+++ BoxCreator.py
@@ -1,11 +1,15 @@
-print("Input x,y values")
-print("X =")
-a = input()
-print("Y= ")
+import random
+print("Input Board Attributes :)")
+print("Width = ")
+w = input()
+print("Height = ")
+h = input()
+print("Number of Bombs =")
 b = input()
 
-x = int(a)
-y = int(b)
+x = int(w)+2
+y = int(h)+2
+z = int(b)
 
 # declaration and adding columns
 board = []
@@ -17,12 +21,12 @@ for j in range(x):
 
 # filling with data
 for i in range(0, x):  # top and bottom row
-    board[i][0] = 1
-    board[i][y - 1] = 1
+    board[i][0] = "B"
+    board[i][y-1] = "B"
 
 for j in range(0, y):  # first and last column
-    board[0][j] = 1
-    board[x - 1][j] = 1
+    board[0][j] = "B"
+    board[x-1][j] = "B"
 
 cols = len(board)
 rows = 0
@@ -32,3 +36,23 @@ for j in range(rows):
     for i in range(cols):
         print(board[i][j], end=" ")
     print()
+
+n=0
+while n<z:
+    e = random.randint(1, x-2)
+    f = random.randint(1, y-2)
+    if(board[e][f] == "X"):
+        n = n
+    else:
+        board[e][f] = "X"
+        n = n + 1
+
+#print()
+
+#rows = 0
+#if cols:
+#    rows = len(board[0])
+#for j in range(rows):
+#    for i in range(cols):
+#        print(board[i][j], end=" ")
+#    print()
