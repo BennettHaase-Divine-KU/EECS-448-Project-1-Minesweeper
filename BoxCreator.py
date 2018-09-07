826cd008fd0798e97a3635544400fb0ee07e1621
bradandrews510 <bradandrews510@gmail.com>
2018-09-07 16:58:52 -0500

    Fixed broken box, now stable with input.

--- BoxCreator.py
+++ BoxCreator.py
@@ -1,29 +1,29 @@
-print("Input x,y values")
-print("X =")
-x=input()
-print("Y= ")
-y = input()
-box(x,y)
+x = 10
+y = 10
 
-def box():
-        board = []
-        for j in range(x):
-        column = []
-                for i in range(y):
-                        column.append(0)
-                board.append(column)
-        # filling with data
-        for i in range(0, x): # top and bottom row
-                board[i][0] = 1
-                board[i][y-1] = 1
-        for j in range(0, y): # first and last column
-                board[0][j] = 1
-                board[x-1][j] = 1
-        cols = len(board)
-        rows = 0
-        if cols:
-                rows = len(board[0])
+board = []
+for j in range(x):
+    column = []
+for i in range(y):
+    column.append(0)
+    board.append(column)
+
+# filling with data
+for i in range(0, x): # top and bottom row
+     board[i][0] = 1
+     board[i][y-1] = 1
+
+for j in range(0, y): # first and last column
+    board[0][j] = 1
+    board[x-1][j] = 1
+
+    cols = len(board)
+    rows = 0
+
+    if cols:
+        rows = len(board[0])
         for j in range(rows):
-                for i in range(cols):
-                        print(board[i][j], end = "")
-                        print()
+            print()
+            for i in range(cols):
+                print(board[i][j], end = "")
+
