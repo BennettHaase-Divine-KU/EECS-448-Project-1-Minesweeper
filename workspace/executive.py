2a152879802e0927410210fedbc7bdce718e2ed7
MrZerg27 <bhaase97@yahoo.com>
2018-09-10 10:26:11 -0500

    Edited executive.py

--- workspace/executive.py
+++ workspace/executive.py
@@ -1,3 +1,16 @@
 class executive:
     def __init__(self, length, width, bombCnt):
         self.gameBoard=[]
+
+    def run(self):
+        self.gameState=0 #gameState 0->unresolved, 1->Win, 2->Loose
+        while self.gameState==0:
+        #Main Game Loop
+
+    def checkWinLoose(self):
+
+    def setUpBoard(self):
+
+    def revealTile(self):
+
+
