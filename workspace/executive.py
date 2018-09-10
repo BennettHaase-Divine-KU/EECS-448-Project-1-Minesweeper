4ae3048c8055ebbcafbb2bea9521027a2cb0ef8d
Bennett Haase-Divine <b.haase-divine@ku.edu>
2018-09-10 14:07:41 -0500

    Fixed up executive.py added tile_test.py

--- workspace/executive.py
+++ workspace/executive.py
@@ -1,16 +1,21 @@
 class executive:
     def __init__(self, length, width, bombCnt):
         self.gameBoard=[]
+        self.length=length
+        self.width=width
 
     def run(self):
         self.gameState=0 #gameState 0->unresolved, 1->Win, 2->Loose
         while self.gameState==0:
         #Main Game Loop
+            continue
+        return
 
     def checkWinLoose(self):
-
+        return
     def setUpBoard(self):
-
+        return
     def revealTile(self):
+        return
 
 
