0c86ed3793a4143efd4d0711a13b6c8aaeed1bf8
Tsung Yu Ho <yuho850616@gmail.com>
2018-09-10 15:36:08 -0500

    Reveal method

--- reveal.py
+++ reveal.py
@@ -1,129 +1,90 @@
-import random
-
-
-class Cla1:
-
-    def __init__(self):
-        self.count = 0
-        self.board = []
-
-    def make_board(self, x, y, z):  # c = x (width) d = y (height) e = z (num bombs)
-        # Declaration of board and adding row/columns
-
-        for j in range(x):
-            column = []
-            for i in range(y):
-                column.append(0)
-            self.board.append(column)
-
-        return
-
-
-    def print_board(self):
-        cols = len(self.board)
-        rows = 0
-        if cols:
-            rows = len(self.board[0])
-        for j in range(rows):
-            for i in range(cols):
-                print(self.board[i][j], end=" ")
-            print()
-
-        return self.board
-
-
-    def place_bomb(self, z):
-        n=0
-        while n<z:
-            e = random.randint(0, x-1)
-            f = random.randint(0, y-1)
-            if(self.board[e][f] == "X"):
-                n = n
-            else:
-                self.board[e][f] = "X"
-                n = n + 1
-
-        return self.board
-
-    def check(self, posx,posy):
-        #global count
-        if self.board[posx][posy]=="*":
-            print("game over")
-        if self.board[posx-1][posy]=="*":
-            self.count+=1
-        if self.board[posx+1][posy]=="*":
-            self.count+=1
-        if self.board[posx][posy-1]=="*":
-            self.count+=1
-        if self.board[posx][posy+1]=="*":
-            self.count+=1
-        if self.board[posx-1][posy-1]=="*":
-            self.count+=1
-        if self.board[posx+1][posy-1]=="*":
-            self.count+=1
-        if self.board[posx-1][posy+1]=="*":
-            self.count+=1
-        if self.board[posx+1][posy+1]=="*":
-            self.count+=1
-
-    def reveal(self, posx,posy):
-        if self.count == "0":
-            reveal(posx,posy-1)
-            reveal(posx,posy+1)
-            reveal(posx-1,posy)
-            reveal(posx+1,posy)
-            reveal(posx-1,posy-1)
-            reveal(posx-1,posy+1)
-            reveal(posx+1,posy-1)
-            reveal(posx+1,posy+1)
-        else:
-            self.board = cla1.check(posx,posy)
-
-    def getcount(self):
-        return(self.count)
-
-    def getboard(self,posx,posy):
-        v = self.board[posx][posy]
-        return(v)
-
-
-print("Welcome to Pysweeper!")
-print("Input Board Attributes :)")
-print("Width = ")
-w = input()
-print("Height = ")
-h = input()
-print("Number of Bombs =")
+print("Input x,y values")
+print("X = ")
+a = input()
+print("Y = ")
 b = input()
-while int(b)>(int(w)*int(h))-1:
-    print("Error enter a valid number of Bombs.")
-    b = input()
+x = int(a)
+y = int(b)
+count = 0
 
-print()
+# declaration and adding columns
+board = []
+for j in range(x):
+    column = []
+    for i in range(y):
+        column.append("0")
+    board.append(column)
 
-x = int(w)  # Width
-y = int(h)  # Height
-z = int(b)  # Number of bombs
+# filling with data
+for i in range(0, x):  # top and bottom row
+    board[1][1] = "*"
+    board[1][3] = "*"
 
+for j in range(0, y):  # first and last column
+    board[3][1] = "*"
+    board[3][3] = "*"
 
-cla1 = Cla1()
-cla1.make_board(x, y, z)
-#cla1.print_board()
-cla1.place_bomb(z)
-print()
-cla1.print_board()
+cols = len(board)
+rows = 0
+if cols:
+    rows = len(board[0])
+for j in range(rows):
+    for i in range(cols):
+        print(board[i][j], end=" ")
+    print()
 
 print("Enter the position you want to reveal")
 print("X =")
-b = input()
-print("Y= ")
 a = input()
-posy = int(a)
-posx = int(b)
+print("Y= ")
+b = input()
+posx = int(a)
+posy = int(b)
 
-cla1.reveal(posx,posy)
-cla1.getboard(posx,posy) = cla1.getcount()
+
+def check(posx,posy):
+    global count
+    if posx>=x or posy>=y or posx<0 or posy<0:
+        print("can't reveal the position out of bound")
+        status = False
+    if board[posx][posy]=="*":
+        print("game over")
+        status = False
+    if board[posx-1][posy]=="*":
+        count+=1
+    if board[posx+1][posy]=="*":
+        count+=1
+    if board[posx][posy-1]=="*":
+        count+=1
+    if board[posx][posy+1]=="*":
+        count+=1
+    if board[posx-1][posy-1]=="*":
+        count+=1
+    if board[posx+1][posy-1]=="*":
+        count+=1
+    if board[posx-1][posy+1]=="*":
+        count+=1
+    if board[posx+1][posy+1]=="*":
+        count+=1
+
+def reveal(posx,posy):
+    if count == "0":
+        reveal(posx,posy-1)
+        reveal(posx,posy+1)
+        reveal(posx-1,posy)
+        reveal(posx+1,posy)
+        reveal(posx-1,posy-1)
+        reveal(posx-1,posy+1)
+        reveal(posx+1,posy-1)
+        reveal(posx+1,posy+1)
+    else:
+        board = check(posx,posy)
+
+
+
+reveal(posx,posy)
+board[posx][posy] = count
 for j in range(rows):
     for i in range(cols):
-        print(self.board[i][j], end=" ")
+        print(board[i][j], end=" ")
     print()
