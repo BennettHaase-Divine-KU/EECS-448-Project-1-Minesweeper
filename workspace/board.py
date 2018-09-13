c2a655b077f9ddccaa904c2932cd325c451dc36a
Bennett Haase-Divine <b.haase-divine@ku.edu>
2018-09-13 17:40:29 -0500

    workspace/board has a fully working reveal and getAdjTile methods

--- workspace/board.py
+++ workspace/board.py
@@ -51,12 +51,8 @@ class Board:
                 self.board[e][f].isBomb = True
                 n = n + 1
         return self.board
-    #TODO set adjacent bombs on tiles
 
-    #reveals a tile at x,y
-    def reveal_tile(self,x_pos,y_pos):
-        #TODO add recursive step to reveal tiles
-        self.board[x_pos][y_pos].isVisible=True;
+
 
     #Prints a debug version of the board to terminal
     def print_board_true(self):
@@ -74,5 +70,89 @@ class Board:
         print()
         return self.board
 
+    def setAdjBomb(self):
+        cols = len(self.board)
+        rows = 0
+        if cols:
+            rows = len(self.board[0])
+        for j in range(rows):
+            for i in range(cols):
+                self.board[i][j].adjBomb=self.search(i,j)
 
+
+
+    #returns the num of adj bombs
+    def search(self, posx, posy):
+        count=0
+
+        if 0 <= posx-1 < self.width and 0 <= posy < self.length:  # make sure is not out of bound
+            if self.board[posx-1][posy].isBomb == True:  # if the adjacent is the bomb
+                count += 1
+
+        if 0 <= posx+1 < self.width and 0 <= posy < self.length:
+            if self.board[posx+1][posy].isBomb == True:
+                count += 1
+
+        if 0 <= posx < self.width and 0 <= posy-1 < self.length:
+            if self.board[posx][posy-1].isBomb == True:
+                count += 1
+
+        if 0 <= posx < self.width and 0 <= posy+1 < self.length:
+            if self.board[posx][posy+1].isBomb == True:
+                count += 1
+
+        if 0 <= posx-1 < self.width and 0 <= posy-1 < self.length:
+            if self.board[posx-1][posy-1].isBomb == True:
+                count += 1
+
+        if 0 <= posx-1 < self.width and 0 <= posy+1 < self.length:
+            if self.board[posx-1][posy+1].isBomb == True:
+                count += 1
+
+        if 0 <= posx+1 < self.width and 0 <= posy-1 < self.length:
+            if self.board[posx+1][posy-1].isBomb == True:
+                count += 1
+
+        if 0 <= posx+1 < self.width and 0 <= posy+1 < self.length:
+            if self.board[posx+1][posy+1].isBomb == True:
+                count += 1
+        return count
+
+    #reveals a tile at x,y
+    def reveal_tile(self, posx, posy):
+        if self.board[posx][posy].adjBomb == 0 and self.board[posx][posy].isVisible==False:  # if the tile you reveal has 0 bomb adjacentand is unrevealed,you can reveal
+            if 0 <= posx-1 < self.width and 0 <= posy < self.length:  # make sure is in the bound
+                self.board[posx][posy].isVisible = True
+                self.reveal_tile(posx - 1, posy)  # go through it again since there is no bomb adjacent on the first tile
+
+            if 0 <= posx+1 < self.width and 0 <= posy < self.length:
+                self.board[posx][posy].isVisible = True
+                self.reveal_tile(posx + 1, posy)
+
+            if 0 <= posx < self.width and 0 <= posy-1 < self.length:
+                self.board[posx][posy].isVisible = True
+                self.reveal_tile(posx, posy - 1)
+
+            if 0 <= posx<self.width and 0 <= posy+1 < self.length:
+                self.board[posx][posy].isVisible = True
+                self.reveal_tile(posx, posy + 1)
+
+            if 0 <= posx-1 < self.width and 0 <= posy-1 < self.length:
+                self.board[posx][posy].isVisible = True
+                self.reveal_tile(posx - 1, posy)
+
+            if 0 <= posx-1 < self.width and 0 <= posy+1 < self.length:
+                self.board[posx][posy].isVisible = True
+                self.reveal_tile(posx - 1, posy - 1)
+
+            if 0 <= posx+1<self.width and 0 <= posy-1 < self.length:
+                self.board[posx][posy].isVisible = True
+                self.reveal_tile(posx + 1, posy - 1)
+
+            if 0 <= posx+1 < self.width and 0 <= posy+1 < self.length:
+                self.board[posx][posy].isVisible = True
+                self.reveal_tile(posx + 1, posy + 1)
+
+        elif(self.board[posx][posy].isVisible==False): #If tile is not revealded but has adjacent bombs just reveal tile
+            self.board[posx][posy].isVisible = True
 
