e7ed4eacd2332cff2d4923bf03bcc91fe5ead776
Matt Cherry <m495c301@ku.edu>
2018-09-13 01:29:34 -0500

    Improved Feature: Clicking any tile reveals it and writes a 1. Later implementation will provide structure off of a game logic 2D list.

--- GUI/gui.py
+++ GUI/gui.py
@@ -5,10 +5,10 @@ import pygame
 
 pygame.init()
 # definition of colors
-WHITE = (255,255,255)
-GREY = (211,211,211)
-BLACK = (0,0,0)
-DARKGREY = (169,169,169)
+WHITE = (255, 255, 255)
+GREY = (211, 211, 211)
+BLACK = (0, 0, 0)
+DARKGREY = (169, 169, 169)
 # tile width and height constant
 WIDTH = 20
 HEIGHT = 20
@@ -16,9 +16,9 @@ HEIGHT = 20
 # margin between tiles
 MARGIN = 5
 
-#calculate screen size based of tile size
+# calculate screen size based of tile size
 pygame.display.init()
-#create the screen surface
+# create the screen surface
 w = input("Width?")
 h = input("Height?")
 screen_width = int(w)
@@ -27,25 +27,45 @@ size = screen_width, screen_height
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Minesweeper")
 
-#main draw loop
+# main draw loop
 program_end = False
-font = pygame.font.SysFont('Ariel',16)
+font = pygame.font.SysFont('Ariel', 16)
 
-#looping multiple rects
-row = screen_height//20
-column = screen_width//20
+# looping multiple rects
+row = (screen_height // 20)
+column = (screen_width // 20)
+
+# game logic grid
+gamelogic = [[0] * row for i in range(column)]
+grid = [[0] * row for i in range(column)]
+
+# TODO: Set the clock rate to a specific FPS
 
 while not program_end:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             program_end = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            c = pos[0] // (WIDTH + MARGIN)
+            r = pos[1] // (HEIGHT + MARGIN)
+            gamelogic[c][r] = 1
+            print("Click", pos, "Grid coordinates: ", r, c)
     screen.fill(DARKGREY)
 
-    grid = [[0] * row for i in range(column) ]
     for i in range(row):
         for j in range(column):
-           grid[j][i]  = pygame.draw.rect(screen,GREY,[(MARGIN+WIDTH)*j+MARGIN,(HEIGHT+MARGIN)*i+MARGIN,WIDTH,HEIGHT])
-           screen.blit(font.render("1",True,BLACK),grid[j][i].center)
+            color = GREY
+
+            grid[j][i] = pygame.draw.rect(screen, color,
+                                          [(MARGIN + WIDTH) * j + MARGIN, (HEIGHT + MARGIN) * i + MARGIN, WIDTH,
+                                           HEIGHT])
+            if gamelogic[j][i] == 1:
+                color = WHITE
+                grid[j][i] = pygame.draw.rect(screen, color,
+                                              [(MARGIN + WIDTH) * j + MARGIN, (HEIGHT + MARGIN) * i + MARGIN, WIDTH,
+                                               HEIGHT])
+                screen.blit(font.render("1", True, BLACK), (grid[j][i]))
     pygame.display.flip()
 
 pygame.quit()
