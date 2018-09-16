a8ec0ffd777247096495371165188987b15f9e6b
Bennett Haase-Divine <b.haase-divine@ku.edu>
2018-09-15 23:04:44 -0500

    Added doxygen documentation to tile.py

--- workspace/tile.py
+++ workspace/tile.py
@@ -1,10 +1,18 @@
+"""@package docstring
+Tile class
+"""
+
 class tile:
+    """Tile class holds important values to be referenced by other classes"""
     def __init__(self):
+        """Default Constructor"""
         self.isBomb=False
         self.isVisible=False
         self.isFlagged=False
         self.adjBomb=0
-    def _init_(self, isBomb, isVisible, isFlaged, adjBomb):
+
+    def  config(self, isBomb, isVisible, isFlaged, adjBomb):
+        """Initialises all values if given them"""
         self.isBomb = isBomb
         self.isVisible = isVisible
         self.isFlagged = isFlaged
