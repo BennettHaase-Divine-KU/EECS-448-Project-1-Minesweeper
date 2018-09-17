f1e9f3517a872ef7b477b1616e330b0e55fe15fc
Matt Cherry <42621769+MattCKU@users.noreply.github.com>
2018-09-16 20:43:27 -0500

    BUG FIX: File path names.

--- Pysweeper.py
+++ Pysweeper.py
@@ -79,7 +79,7 @@ while (incorrect == True):
         break;
     except ValueError:
         badCase = Tk()
-        badCase.iconbitmap('MemoryLeakLogo.ico')
+        badCase.iconbitmap('GUI\MemoryLeakLogo.ico')
         Label(badCase, text="Please enter a valid integer.\n1<Width<73 and 1<Height<41\nNumber of bombs smaller than width*height\nEmpty tiles <1089", ).grid(row=0)
         Button(badCase, text="Ok", command=badCase.destroy).grid(row=1)
         badCase.mainloop()
