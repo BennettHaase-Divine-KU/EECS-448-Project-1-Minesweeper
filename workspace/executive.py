f26d56a62b06d853f65950dc32b2bbbc9af61a52
Bennett Haase-Divine <b.haase-divine@ku.edu>
2018-09-16 19:26:44 -0500

    Cleaned up commented code

--- workspace/executive.py
+++ workspace/executive.py
@@ -58,30 +58,6 @@ class executive:
         self.gameBoard.place_bomb()
         self.gameBoard.setAdjBomb()
         self.gameBoard.print_board_true()
-        """
-        self.gameState=0 #gameState 0->unresolved, 1->Win, 2->Loose
-        while self.gameState==0:
-            self.gameBoard.print_board_true()
-            self.gameBoard.print_board()
-
-
-            #Main Game Loop
-            print("Select a spot to reveal")
-            print("x pos:")
-            x=int(input())
-            print("y pos:")
-            y=int(input())
-
-            self.gameBoard.reveal_tile(x,y)
-            self.gameState=self.checkWinLose()
-            continue
-       
-        self.gameBoard.print_board()
-        if(self.gameState==2):
-            print("YOU LOSE")
-        elif(self.gameState==1):
-            print("YOU WIN")
-         """
         return
 
 
